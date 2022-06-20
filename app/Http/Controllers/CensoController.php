42e493965b003e85de7ecb475bcdbe2ff8594125
gabriela <mm15136@ues.edu.sv>
2022-06-20 11:09:50 -0600

    Documentacion en Swgger

--- app/Http/Controllers/CensoController.php
+++ app/Http/Controllers/CensoController.php
@@ -1,103 +1,88 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Censo;
 
 use Illuminate\Http\Request;
 
+/**
+ * CensoController
+ * @package App\Http\Controllers\CensoController
+ * @OA\OpenApi(
+ *     @OA\Info(
+ *         version="1.0.0",
+ *         title="Swagger Test",
+ *         @OA\License(name="MIT")
+ *     ),
+ *     @OA\Server(
+ *         description="API server",
+ *         url="http://127.0.0.1:8000/",
+ *     ),
+ * )
+ */
+
 class CensoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+ 
+    /**                                                                                             
+ * @OA\Get(                                                                                         
+ *     path="/censos",                                                                                    
+ *     description="Home page",                                                                             
+ *     @OA\Response(response="default", description="Mostrando datos de los censos")                                 
+ * )                                                                                                
+ */                                                                                                 
+   
     public function index()
     {
 
-
-       $censos= Censo::select('censos.id','personas.*', 'municipios.nombre_municipio')->join('personas','persona_id','=','personas.id')->join('municipios','municipio_id','=','municipios.id')->get();
-       return $censos;
+        $censos = Censo::select('censos.id', 'personas.*', 'municipios.nombre_municipio')->join('personas', 'persona_id', '=', 'personas.id')->join('municipios', 'municipio_id', '=', 'municipios.id')->get();
+        return $censos;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+  
     public function create()
     {
-        //
+        
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function store(Request $request)
     {
-        //
+       
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-
+  
 
     public function show($dui)
     {
-      $censosss= DB::table('censos')->join('personas','persona_id','=','persona.id')->get();
-
-
 
         $censos = Persona::where('dui', $dui)->get([
             'nombre',
             'apellido',
             'fecha_nacimiento',
             'dui',
-            'estado_civil'
+            'estado_civil',
         ]);
 
         return $censosss;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
-        //
+        
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function update(Request $request, $id)
     {
-        //
+       
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+  
     public function destroy($id)
     {
-        //
+       
     }
 }
--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -3,40 +3,73 @@
 namespace App\Http\Controllers;
 use App\Models\Persona;
 use Illuminate\Http\Request;
+use App\Http\Request\PersonaRequest;
 
-use App\Http\Requests\PersonaRequest as RequestsPersonaRequest;
+
 
 class PersonaController extends Controller
 {
+   
+    public $GetFilter = [
+        "id",
+        "nombre",
+        "apellido",
+        "fecha_nacimiento",
+        "dui",
+        "estado_civil",
+    ];
+
 
     public function getByDui($dui) {
+        $persona = Persona::Where("dui", $dui)->get($this->GetFilter)->first();
+        $edad= $this->busca_edad($persona->fecha_nacimiento);
 
+        $info= array($persona,$edad);
+
+        return response()->json($info);
     }
+ /**
+ * @OA\Post(
+ *   path="/persona/new",
+ *   summary="Form post",
+ *   @OA\RequestBody(
+ *     @OA\MediaType(
+ *       mediaType="multipart/form-data",
+ *       @OA\Schema(
+ *         @OA\Property(property="nombre"),
+ *         @OA\Property(property="apellido"),
+ *         @OA\Property(property="Fecha de nacimiento"),
+ *         @OA\Property(property="dui"),
+ *         @OA\Property(property="estado civil"),
+ *         @OA\Property(
+ *           description="file to upload",
+ *           type="string",
+ *           format="binary",
+ *         ),
+ *       )
+ *     )
+ *   ),
+ *   @OA\Response(response=200, description="Success")
+ * )
+ */
 
-    public function create(RequestsPersonaRequest $request) {
-        //validando la edad del usuario
-        $edad= $this->busca_edad($request->fecha_nacimiento);
-       if($edad<18){
-             $request->dui="";
-        }
+
+    public function created(Request $request) {
+        
+   
         $persona = new Persona([
-            "nombres" => $request->nombres,
-            "apellidos" => $request->apellidos,
+            "nombre" => $request->nombre,
+            "apellido" => $request->apellido,
             "fecha_nacimiento" => $request->fecha_nacimiento,
             "dui" => $request->dui,
-            "estado_civil" => $request->estado_civil,
-
+            "estado_civil" => $request->estado_civil,    
+    
         ]);
 
         $persona->save();
-        /*$edad= $this->busca_edad($persona->fecha_nacimiento);
 
-        $info= array($persona,$edad);*/
 
-        return response()->json([
-            'message'=>'Informacion agregada con exito',
-            'persona'=> $persona,
-        ]);
+        return response()->json($persona);
     }
 
 
@@ -47,29 +80,30 @@ function busca_edad($fecha_nacimiento){
         $dia=date("d");
         $mes=date("m");
         $ano=date("Y");
-
-
+        
+        
         $dianaz=date("d",strtotime($fecha_nacimiento));
         $mesnaz=date("m",strtotime($fecha_nacimiento));
         $anonaz=date("Y",strtotime($fecha_nacimiento));
-
-
-        //si el mes es el mismo pero el día inferior aun no ha cumplido años, le quitaremos un año al actual
-
+        
+        
+        
         if (($mesnaz == $mes) && ($dianaz > $dia)) {
         $ano=($ano-1); }
-
-        //si el mes es superior al actual tampoco habrá cumplido años, por eso le quitamos un año al actual
-
+        
+     
+        
         if ($mesnaz > $mes) {
         $ano=($ano-1);}
-
-         //ya no habría mas condiciones, ahora simplemente restamos los años y mostramos el resultado como su edad
-
+        
+   
+        
         $edad=($ano-$anonaz);
-
-
+        
+        
         return $edad;
+        
+        
         }
 
 }
